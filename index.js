3a0baf66d1915e9f8d9f836a71156c43ec3fe922
joshey40 <joshuapieniak@gmail.com>
2024-05-01 11:35:42 +0200

    test

--- index.js
+++ index.js
@@ -11,12 +11,29 @@ webApp.use('/res', express.static(__dirname + '/res'));
 
 webApp.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
 webApp.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
+
+var req = new XMLHttpRequest();
+req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+        console.log(req.responseText);
+        var data = JSON.parse(req.responseText);
+        officialCards = data.officialCards;
+        customCards = data.customCards;
+        addAllEffectTags();
+        addAllCustomCharacters();
+        applyFilters();
+    }
+};
+req.open('GET', CARDS_API_URL, true);
+req.send(null);
 
 // Done
 webApp.listen(process.env.PORT || port, () => {
