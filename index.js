54c483f2f3e663c35298ec605699df8200710527
joshey40 <joshuapieniak@gmail.com>
2024-05-01 12:00:20 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Web Server (index.html, main.js, style.css)
 const express = require('express');
-const request = require('request');
 const { join } = require('path');
 const webApp = express();
 const port = 3000;
@@ -10,29 +9,14 @@ webApp.use('/js', express.static(__dirname + '/js'));
 webApp.use('/css', express.static(__dirname + '/css'));
 webApp.use('/res', express.static(__dirname + '/res'));
 
-webApp.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
 webApp.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-  
-  webApp.get('/jokes/random', (req, res) => {
-    request(
-      { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
-      (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          return res.status(500).json({ type: 'error', message: err.message });
-        }
-  
-        res.json(JSON.parse(body));
-      }
-    )
-  });
 
-
+webApp.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
 
 // Done
 webApp.listen(process.env.PORT || port, () => {
